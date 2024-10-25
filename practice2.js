88e8b393c3070f4958123fc82f0459161daf58aa
danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-25 16:57:54 -0500

    fix bug

--- practice2.js
+++ practice2.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded",() => {
 })
 function runcode(){
     document.getElementById("submit").onclick = function(){
-    let redtext = document.getElementById("redtex").style.color;
+    let redtext = document.getElementById("redtext").style.color;
     let bluetext = document.getElementById("bluetext").style.color;
     let greentext = document.getElementById("greentext").style.color;
     document.getElementById("dummy").style.color = "rgb("+redtext+", "+greentext+", "+bluetext+");"
