dd496155041bfcaa2d6efbe1b78dae6580a7d5c6
danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-27 19:34:17 -0500

    fix

--- practice4.php
+++ practice4.php
@@ -4,11 +4,12 @@ $size = $_POST["size"];
 echo "<table>";
     for ($i=0; $i < $size; $i++) { 
 	echo "<tr>";
-	for ($g=0; $g < $size; $g) { 
+	for ($g=0; $g < $size; $g++) { 
 	    $temp = $g*$i;
 	    echo "<td>$temp</td>";
 	}
 	echo "</tr>";
-}
+    }
+echo "<table>"
 ?>
 
