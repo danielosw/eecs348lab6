fc8a6c93081d59a27aa21280c6981e9030e2e3e3
danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-27 19:22:48 -0500

    test 2

--- practice4.php
+++ practice4.php
@@ -1,12 +1,3 @@
-<html>
-<div class="wrapper">
-<head>
-    <link rel="stylesheet" type="text/css" href="practice.css" />
-    <h1 id="header">Multiplication Table</h1>
-</head>
 
-<body id="body">
-<?php $size?>
-</body>
-</div>
-</html>
+<?php echo $_POST[0] ?>
+
