50fdac5f1c42b3b2a3d394a3d5b1f0706082ab28
danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-27 19:40:05 -0500

    fix border

--- practice4.php
+++ practice4.php
@@ -1,11 +1,6 @@
-<style>
-table{
-border: 2rem;
-}
-</style>
 <?php 
 $size = $_POST["size"];
-echo "<table>";
+echo "<table border=\"1\">";
     for ($i=1; $i < $size+1; $i++) { 
 	echo "<tr>";
 	for ($g=1; $g < $size+1; $g++) { 
